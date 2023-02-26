739084b2a27940ad588938cadbcfc9460b16601c
radewahid <radenwahiddd29@gmail.com>
2023-02-26 21:10:13 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,41 @@
+//nav
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const scroll = document.querySelector('#scroll');
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+        scroll.classList.remove('hidden');
+        scroll.classList.add('flex');
+    } else{
+        header.classList.remove('navbar-fixed');
+        scroll.classList.remove('flex');
+        scroll.classList.add('hidden');
+    }
+};
+// ham
+
+const ham = document.querySelector('#ham');
+const navMenu = document.querySelector('#nav-menu');
+ham.addEventListener('click', function(){
+ ham.classList.toggle('ham-active');
+ navMenu.classList.toggle('hidden');
+});
+
+//click
+window.addEventListener('click', function(e) {
+    if(e.target !=ham && e.target != navMenu){
+        ham.classList.remove('ham-active'); 
+        navMenu.classList.add('hidden');
+
+    }
+    
+});
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('#html');
+darkToggle.addEventListener('click', function() {
+    darkToggle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  // ...
+  content: ["./Public/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    
+  
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      '4xl': '2rem',
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
